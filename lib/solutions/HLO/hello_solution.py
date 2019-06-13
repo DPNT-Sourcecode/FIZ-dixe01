fa13fa0d832cbef0491715e1892ebfaace3ec825
sbx_user1051 <sbx_user1051@169.254.10.117>
2019-06-13 12:23:45 +0000

    Thu Jun 13 12:23:45 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,7 +1,4 @@
-
-
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
     return "Hello, %s" % friend_name
-
---
+++ test/solution_tests/HLO/test_hello.py
@@ -0,0 +1,7 @@
+import unittest
+from solutions.HLO import hello_solution
+
+class HelloTest(unittest.TestCase):
+
+    def test_says_hello_to_friend(self):
+        self.assertEqual(hello_solution.hello('David'), 'Hello, David')
