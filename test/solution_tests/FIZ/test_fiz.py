29989c0ba38823c291bd2c66bc39bab364e0a23a
sbx_user1051 <sbx_user1051@169.254.10.117>
2019-06-13 13:02:18 +0000

    Thu Jun 13 13:02:18 UTC 2019

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -28,14 +28,19 @@ class FizTest(unittest.TestCase):
     def test_returns_fizz_buzz_for_nums_with_5_and_3(self):
         self.assertEqual(fb.fizz_buzz(352), 'fizz buzz')
 
-    def test_adds_deluxe_when_deluxe(self):
-        self.assertEqual(fb.fizz_buzz(22), 'deluxe')
-        self.assertEqual(fb.fizz_buzz(222), 'fizz deluxe')
+    def test_can_detect_new_deluxe(self):
+        self.assertEqual(fb.deluxe(36), True)
+        self.assertEqual(fb.deluxe(35), True)
 
-    def test_fake_deluxe(self):
-        self.assertEqual(fb.fizz_buzz(11), 'fake deluxe')
-        self.assertEqual(fb.fizz_buzz(333), 'fizz fake deluxe')
-        self.assertEqual(fb.fizz_buzz(555), 'fizz buzz fake deluxe')
+    # def test_adds_deluxe_when_deluxe(self):
+    #     self.assertEqual(fb.fizz_buzz(22), 'deluxe')
+    #     self.assertEqual(fb.fizz_buzz(222), 'fizz deluxe')
+
+    # def test_fake_deluxe(self):
+    #     self.assertEqual(fb.fizz_buzz(11), 'fake deluxe')
+    #     self.assertEqual(fb.fizz_buzz(333), 'fizz fake deluxe')
+    #     self.assertEqual(fb.fizz_buzz(555), 'fizz buzz fake deluxe')
+
 
 
 
