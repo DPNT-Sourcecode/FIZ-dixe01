72c3f733be020308154654245ca4473cdaddd8e3
sbx_user1051 <sbx_user1051@169.254.10.117>
2019-06-13 12:41:43 +0000

    Thu Jun 13 12:41:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,9 +1,9 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
     output = ""
-    if number % 3 == 0:
+    if number % 3 == 0 or '3' in str(number):
         output += "fizz "
-    if number % 5 == 0:
+    if number % 5 == 0 or '5' in str(number):
         output += "buzz "
     if output == "":
         return str(number)
--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -6,13 +6,21 @@ class FizTest(unittest.TestCase):
     def test_returns_number_as_string(self):
         self.assertEqual(fb.fizz_buzz(2), '2')
 
-    def test_returns_fizz_for_3s(self):
+    def test_returns_fizz_for_3_multiples(self):
         self.assertEqual(fb.fizz_buzz(3), 'fizz')
         self.assertEqual(fb.fizz_buzz(6), 'fizz')
         self.assertEqual(fb.fizz_buzz(999), 'fizz')
 
-    def test_returns_buzz_for_5s(self):
+    def test_returns_buzz_for_5_multiples(self):
         self.assertEqual(fb.fizz_buzz(25), 'buzz')
 
-    def test_returns_fizz_buzz_for_15s(self):
+    def test_returns_fizz_buzz_for_15_multiples(self):
         self.assertEqual(fb.fizz_buzz(30), 'fizz buzz')
+
+    def test_returns_fizz_for_nums_with_3_in(self):
+        self.assertEqual(fb.fizz_buzz(13), 'fizz')
+        self.assertEqual(fb.fizz_buzz(301), 'fizz')
+
+    def test_returns_buzz_for_nums_with_5_in(self):
+        self.assertEqual(fb.fizz_buzz(51), 'buzz')
+        self.assertEqual(fb.fizz_buzz(501), 'buzz')
