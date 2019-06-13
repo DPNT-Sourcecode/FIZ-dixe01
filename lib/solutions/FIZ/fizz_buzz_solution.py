9dada6038f4747fbec865abe1e086f27d42392a5
sbx_user1051 <sbx_user1051@169.254.10.117>
2019-06-13 12:37:43 +0000

    Thu Jun 13 12:37:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,5 +1,10 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+    output = ""
     if number % 3 == 0:
-        return "fizz"
-    return str(number)
+        output += "fizz"
+    if number % 5 == 0:
+        output += "buzz"
+    if output == "":
+        return str(number)
+
