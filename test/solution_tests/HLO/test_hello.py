d404f2c3294d5e6b4858bb0139c348e64a4d6178
sbx_user1051 <sbx_user1051@169.254.10.117>
2019-06-13 12:26:42 +0000

    Thu Jun 13 12:26:42 UTC 2019

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,7 +3,8 @@ from lib.solutions.HLO import hello_solution
 
 class HelloTest(unittest.TestCase):
 
-    def test_says_hello_to_world(self):
-        self.assertEqual(hello_solution.hello('David'), 'Hello, World!')
+    def test_says_hello_to_friend(self):
+        self.assertEqual(hello_solution.hello('David'), 'Hello, David')
+
 
 
